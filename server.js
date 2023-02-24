85e95b3d504f8c7fd3111799f63e1ed51e9f4025
Moinak <moinak.dey8@gmail.com>
2023-02-24 20:45:30 +0530

    set the same site cookie

--- server.js
+++ server.js
@@ -23,14 +23,11 @@ app.use(
 	})
 );
 
-app.set("trust proxy", 1); // trust first proxy
-
 app.use(
 	session({
 		secret: "thisismysecret.",
 		saveUninitialized: false,
 		resave: false,
-		cookie: { secure: false },
 	})
 );
 app.use(passport.initialize());
@@ -41,13 +38,7 @@ app.get("/", (req, res) => {
 	res.redirect("http://localhost:3000");
 });
 
-app.get("/auth/google", passport.authenticate("google", { scope: ["profile"] }), (req, res) => {
-	res.cookie("connect.sid", "gopon-backend.vercel.app/", {
-		sameSite: "None",
-		secure: false,
-		maxAge: 3600000,
-	});
-});
+app.get("/auth/google", passport.authenticate("google", { scope: ["profile"] }));
 
 app.get(
 	"/auth/google/callback",
@@ -58,6 +49,11 @@ app.get(
 );
 
 app.get("/authenticate", (req, res) => {
+	res.cookie("connect.sid", "gopon-backend.vercel.app/", {
+		sameSite: "None",
+		secure: true,
+		maxAge: 3600000,
+	});
 	if (req.isAuthenticated()) {
 		res.send(true);
 	} else {
@@ -77,6 +73,11 @@ app.get("/secrets", (req, res) => {
 
 app.get("/user", (req, res) => {
 	console.log(req.user);
+	res.cookie("connect.sid", "gopon-backend.vercel.app/", {
+		sameSite: "None",
+		secure: true,
+		maxAge: 3600000,
+	});
 	res.send(req.user);
 });
 
