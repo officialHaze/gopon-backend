55a7b55d6f6f399e4c8394cdd366e2171fb10241
Moinak <moinak.dey8@gmail.com>
2023-02-24 22:00:06 +0530

    frustrating commit 5

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const passport = require("passport");
-const session = require("express-session");
+const session = require("cookie-session");
 const cors = require("cors");
 const getSecrets = require("./getSecrets");
 const uploadSecret = require("./uploadSecret");
