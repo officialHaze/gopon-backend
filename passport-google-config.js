10053b99e44b4a5c308d950dada49ce2219c4f47
Moinak <moinak.dey8@gmail.com>
2023-02-24 19:08:10 +0530

    added live server link as the call back url for google verification

--- passport-google-config.js
+++ passport-google-config.js
@@ -8,7 +8,7 @@ passport.use(
 		{
 			clientID: process.env.GOOGLE_CLIENT_ID,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-			callbackURL: "http://localhost:5000/auth/google/callback",
+			callbackURL: "https://gopon-backend.vercel.app/auth/google/callback",
 			scope: ["profile"],
 			state: true,
 		},
