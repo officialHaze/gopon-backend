2718f024c426c15b1f76d0f4e9e1fb507725ad0c
Moinak <moinak.dey8@gmail.com>
2023-02-24 16:18:09 +0530

    changed google call url to actual render server site link

--- passport-google-config.js
+++ passport-google-config.js
@@ -7,7 +7,7 @@ passport.use(
 		{
 			clientID: process.env.GOOGLE_CLIENT_ID,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-			callbackURL: "http://localhost:5000/auth/google/callback",
+			callbackURL: "https://gopon-backend-officialhaze-moinak.onrender.com/auth/google/callback",
 			scope: ["profile"],
 			state: true,
 		},
